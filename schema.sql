3fd6ea63df18c9dc6e5a6b8312a1f7fc73ee02ea
rawat-is-here <akashjirawati3@gmail.com>
2026-08-17 13:03:52 +0530

    Updated table

--- schema.sql
+++ schema.sql
@@ -1,3 +1,6 @@
+-- WARNING: This schema is for context only and is not meant to be run.
+-- Table order and constraints may not be valid for execution.
+
 CREATE TABLE public.tigers (
   id text NOT NULL,
   name text,
@@ -6,25 +9,26 @@ CREATE TABLE public.tigers (
 );
 CREATE TABLE public.captures (
   id bigint GENERATED ALWAYS AS IDENTITY NOT NULL,
-  tiger_id text,
-  image_path text NOT NULL UNIQUE,
-  station text NOT NULL,
-  timestamp timestamp with time zone NOT NULL,
-  latitude double precision NOT NULL,
-  longitude double precision NOT NULL,
-  status text NOT NULL CHECK (status = ANY (ARRAY['processed'::text, 'quarantined'::text, 'pending_review'::text])),
-  confidence double precision NOT NULL,
+  tiger_id text NOT NULL,
+  image_path text,
+  station text,
+  timestamp timestamp with time zone DEFAULT timezone('utc'::text, now()),
+  latitude double precision,
+  longitude double precision,
+  status text,
+  confidence double precision,
+  embedding USER-DEFINED,
   CONSTRAINT captures_pkey PRIMARY KEY (id),
   CONSTRAINT captures_tiger_id_fkey FOREIGN KEY (tiger_id) REFERENCES public.tigers(id)
 );
 CREATE TABLE public.alerts (
   id bigint GENERATED ALWAYS AS IDENTITY NOT NULL,
-  tiger_id text,
-  alert_type text NOT NULL,
-  severity text NOT NULL CHECK (severity = ANY (ARRAY['CRITICAL'::text, 'WARNING'::text, 'INFO'::text])),
-  message text NOT NULL,
-  timestamp timestamp with time zone NOT NULL DEFAULT timezone('utc'::text, now()),
-  resolved boolean NOT NULL DEFAULT false,
+  tiger_id text NOT NULL,
+  alert_type text,
+  severity text,
+  message text,
+  timestamp timestamp with time zone DEFAULT timezone('utc'::text, now()),
+  resolved boolean DEFAULT false,
   evidence jsonb,
   CONSTRAINT alerts_pkey PRIMARY KEY (id),
   CONSTRAINT alerts_tiger_id_fkey FOREIGN KEY (tiger_id) REFERENCES public.tigers(id)
